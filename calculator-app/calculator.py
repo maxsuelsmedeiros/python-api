47e250db8e84ac83aa403726136bec7698cab097
maxsuelsmedeiros <maxsuelmedeiros@hotmail.com>
2024-06-03 22:56:33 -0300

    adding the tests for the operations of the calculator

--- calculator-app/calculator.py
+++ calculator-app/calculator.py
@@ -6,3 +6,15 @@
 def calculator(operation: str,number_1 : int|float,number_2 : int|float):
     if operation is None:
         raise Exception("Operation can't be None.")
+    if number_1 is None or number_2 is None:
+        raise Exception('Need to inform 2 valid numbers.')
+    if operation not in ['add','sub','div','multiply']:
+        raise Exception('The operations can be add,sub,div or multiply.')
+    if operation == 'add':
+        return number_1 + number_2
+    elif operation == 'sub':
+        return number_1 - number_2
+    elif operation == 'div':
+        return number_1/number_2
+    elif operation == 'multiply':
+        return number_1 * number_2
--- calculator-app/test_calculator.py
+++ calculator-app/test_calculator.py
@@ -7,6 +7,35 @@ class TestCalculator(unittest.TestCase):
         with self.assertRaises(Exception) as ex:
             calculator(None,2,2)
         self.assertEqual(ex.exception.args[0], second="Operation can't be None.")
+    
+    def test_valid_number(self):
+        with self.assertRaises(Exception) as ex:
+            calculator('add',None,2)
+        self.assertEqual(ex.exception.args[0], 'Need to inform 2 valid numbers.')
+        with self.assertRaises(Exception) as ex:
+            calculator('add',2,None)
+        self.assertEqual(ex.exception.args[0], 'Need to inform 2 valid numbers.')
+
+    def test_operation_isvalid(self):
+        with self.assertRaises(Exception) as ex:
+            calculator('fatorial',2,10)
+        self.assertEqual(ex.exception.args[0], 'The operations can be add,sub,div or multiply.')
+
+    def test_add(self):
+        result: float = calculator('add',10,30)
+        self.assertEqual(result,40)
+
+    def test_sub(self):
+        result: float = calculator('sub',30,10)
+        self.assertEqual(result,20)
+
+    def test_div(self):
+        result: float = calculator('div',30,10)
+        self.assertEqual(result,3)
+
+    def test_multiply(self):
+        result: float = calculator('multiply',5,8)
+        self.assertEqual(result,40)
 
 if __name__=='__main__':
     unittest.main()
