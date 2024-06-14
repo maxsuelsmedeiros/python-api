87400204df96ad87ff0d6a1e21a1a788171421d5
maxsuelsmedeiros <maxsuelmedeiros@hotmail.com>
2024-06-13 22:24:47 -0300

    'experimenting with json and POST'

--- API/app.py
+++ API/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask , jsonify, request as flask_request
 import json
 import typing
 
@@ -6,7 +6,9 @@ import typing
 type json = json
 app=Flask(__name__)
 
-purchase_orders: json = [
+
+global purchase_orders 
+purchase_orders: json= [
     {
         'id':1,
         'description':'purchase order 1',
@@ -31,16 +33,30 @@ def home() -> str:
     return 'Hello Beautiful World!'
 
 @app.route(rule='/purchase_orders')
-def get_purchase_orders() -> jsonify:
+def get_purchase_orders() -> json:
     return jsonify(purchase_orders)
 
 @app.route(rule='/purchase_orders/<int:id>')
-def get_purchase_orders_by_id(id) -> json|str:
+def get_purchase_orders_by_id(id) -> json:
     for purchase_item in purchase_orders:
         if purchase_item['id']==id:
             return jsonify(purchase_item)
     return jsonify({'message':'The ID passed:{} is not valid, review it and try again!'.format(id)})
 
-
+@app.route('/purchase_orders', methods=['POST'])
+def create_purchase_order() -> json:
+    request_to_add: json = flask_request.get_json()
+    purchase_order: json = {
+        "id": request_to_add["id"],
+        "description": request_to_add["description"],
+        "items": [{
+            "id":request_to_add["items"][0]["id"],
+            "description":request_to_add["items"][0]["description"],
+            "value":request_to_add["items"][0]["value"]
+        }]
+    }
+    # adding the purchase order in the purchase_orders
+    purchase_orders.append(purchase_order)
+    return jsonify(purchase_order)
 
 app.run(port=5000)
