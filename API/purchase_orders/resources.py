87b503c6c9096e6f4315ae2d901aee4553995d95
maxsuelsmedeiros <maxsuelmedeiros@hotmail.com>
2024-08-20 23:37:10 -0300

    'refactoring the resources with the POST method and testing it'

--- API/purchase_orders/resources.py
+++ API/purchase_orders/resources.py
@@ -1,6 +1,7 @@
 from flask import jsonify
-from flask_restful import Resource
+from flask_restful import Resource,reqparse
 from typing import Union, Dict, List,Any,Type
+
 
 JSON = Union[Dict[str, Any], List[Any], int, str, float, bool, Type[None]]
 global purchase_orders 
@@ -19,5 +20,32 @@ purchase_orders: JSON= [
 ]
 
 class PurchaseOrders(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'id',
+        type = int,
+        required = True,
+        help='Id not valid, inform a new one!'
+    )
+
+    parser.add_argument(
+        'description',
+        type = str,
+        required = True,
+        help='Description not valid, inform a new one!'
+    )
+
+
     def get(self)-> JSON:
         return jsonify(purchase_orders)
+    
+    def post(self)-> JSON:
+        data = PurchaseOrders.parser.parse_args()
+        purchase_order = {
+            'id': data['id'],
+            'description': data['description'],
+            'items': []
+        }
+        purchase_orders.append(purchase_order)
+
+        return jsonify(purchase_order)
